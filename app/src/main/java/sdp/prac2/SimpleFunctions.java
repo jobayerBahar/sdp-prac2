1f313df77e87c6f1c7d09b2b06e0e08e0772a436
Antony Baker <G23B9640@ict.ru.ac.za>
2025-04-10 15:21:54 +0200

    Added Task2 function

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,19 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+
+    public <T> List<String> Task2(List<T> nested) {
+        List<String> result = new ArrayList<String>();
+        for (int i = 0; i < nested.size(); i++) {
+            if (nested.get(i).toString().length() != 0) {
+                String item = nested.get(i).toString();
+                item = item.substring(1, item.length() - 1);
+                result.add(item);
+            }
+        }
+        return result;
+    }
+
+    
+
 }
